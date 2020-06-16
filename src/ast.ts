e3f6baf781bfd593eb03f6aaf7ca1c6c060f6c51
Tim Jentzsch <Tim Jentzsch>
2020-06-16 09:48:02 +0200

    Initial commit

---
+++ src/ast.ts
@@ -0,0 +1,40 @@
+export type ASTNode = BaseASTNode;
+
+export interface BaseASTNode {
+  readonly type: 'object' | 'property' | 'string' | 'number';
+  readonly parent?: ASTNode;
+  readonly offset: number;
+  readonly length: number;
+  readonly children?: ASTNode[];
+  readonly value?: string | number | null;
+}
+
+export interface PropertyASTNode extends BaseASTNode {
+  readonly type: 'property';
+  readonly keyNode: ASTNode;
+  readonly valueNode?: ASTNode;
+  readonly children: ASTNode[];
+}
+
+export interface ObjectASTNode extends BaseASTNode {
+  readonly type: 'object';
+  readonly properties: PropertyASTNode[];
+  readonly children: ASTNode[];
+}
+
+export interface LiteralASTNode extends BaseASTNode {
+  readonly type: 'string' | 'number';
+  readonly quoted: boolean;
+  readonly value: string | number;
+}
+
+export interface StringASTNode extends LiteralASTNode {
+  readonly type: 'string';
+  readonly value: string;
+}
+
+export interface NumberASTNode extends LiteralASTNode {
+  readonly type: 'number';
+  readonly value: number;
+  readonly isInteger: boolean;
+}
