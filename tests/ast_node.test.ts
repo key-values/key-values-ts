8908f6c82560a7870dc61fd961fb1c7b542ae33f
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-06-19 14:19:11 +0200

    Add function to find node at a given offset

--- tests/ast_node.test.ts
+++ tests/ast_node.test.ts
@@ -2,10 +2,12 @@ import {
   StringASTNodeImpl,
   PropertyASTNodeImpl,
   ObjectASTNodeImpl,
+  NodePositionImpl,
 } from '../src/ast_node_impl';
-import { next } from '../src/ast_node';
+import { next, findAtOffset } from '../src/ast_node';
 
 describe('ASTNode', () => {
+  // Next
   describe('next', () => {
     test('should be null for simple string node', () => {
       const node = new StringASTNodeImpl('value');
@@ -53,6 +55,117 @@ describe('ASTNode', () => {
       expect(next(value)).toBe(null);
     });
   });
+  // Find at offset
+  describe('findAtOffset', () => {
+    describe('string node', () => {
+      test('should be string for offset in string', () => {
+        const str = new StringASTNodeImpl('value', createStartLinePos(7));
+
+        expect(findAtOffset(str, 0)).toBe(str);
+        expect(findAtOffset(str, 3)).toBe(str);
+        expect(findAtOffset(str, 7)).toBe(str);
+      });
+      test('should be null for offset before string', () => {
+        const str = new StringASTNodeImpl('value', createStartLinePos(7));
+
+        expect(findAtOffset(str, -1)).toBe(null);
+      });
+      test('should be null for offset after string', () => {
+        const str = new StringASTNodeImpl('value', createStartLinePos(7));
+
+        expect(findAtOffset(str, 8)).toBe(null);
+      });
+    });
+    describe('property node', () => {
+      test('should be key for offset in property key', () => {
+        // "key"  "value"
+        const key = new StringASTNodeImpl('key', createStartLinePos(5));
+        const value = new StringASTNodeImpl('value', createLinePos(7, 7, 1, 7));
+        const property = new PropertyASTNodeImpl(
+          key,
+          value,
+          createStartLinePos(14)
+        );
+        expect(findAtOffset(property, 0)).toBe(key);
+        expect(findAtOffset(property, 2)).toBe(key);
+        expect(findAtOffset(property, 4)).toBe(key);
+      });
+      test('should be value for offset in property value', () => {
+        // "key"  "value"
+        const key = new StringASTNodeImpl('key', createStartLinePos(5));
+        const value = new StringASTNodeImpl('value', createLinePos(7, 7, 1, 7));
+        const property = new PropertyASTNodeImpl(
+          key,
+          value,
+          createStartLinePos(14)
+        );
+        expect(findAtOffset(property, 7)).toBe(value);
+        expect(findAtOffset(property, 9)).toBe(value);
+        expect(findAtOffset(property, 13)).toBe(value);
+      });
+      test('should be property for offset in between key and value', () => {
+        const key = new StringASTNodeImpl('key', createStartLinePos(5));
+        const value = new StringASTNodeImpl('value', createLinePos(7, 7));
+        // "key"  "value"
+        const property = new PropertyASTNodeImpl(
+          key,
+          value,
+          createStartLinePos(14)
+        );
+        expect(findAtOffset(property, 6)).toBe(property);
+      });
+    });
+    describe('object node', () => {
+      test('should be object for offset in empty object', () => {
+        // {}
+        const obj = new ObjectASTNodeImpl([], createStartLinePos(2));
+
+        expect(findAtOffset(obj, 0)).toBe(obj);
+        expect(findAtOffset(obj, 1)).toBe(obj);
+        expect(findAtOffset(obj, 2)).toBe(obj);
+      });
+      test('should be property key for offset in property key for single line object', () => {
+        const key = new StringASTNodeImpl('key', createLinePos(2, 5));
+        const value = new StringASTNodeImpl('value', createLinePos(9, 7));
+        // __"key"  "value"
+        const property = new PropertyASTNodeImpl(
+          key,
+          value,
+          createStartLinePos(14)
+        );
+        // { "key"  "value" }
+        const obj = new ObjectASTNodeImpl([property], createStartLinePos(18));
+
+        expect(findAtOffset(obj, 2)).toBe(key);
+        expect(findAtOffset(obj, 4)).toBe(key);
+        expect(findAtOffset(obj, 7)).toBe(key);
+      });
+      test('should be object for offset in between property keys', () => {
+        // __"key"  "value"
+        const property1 = new PropertyASTNodeImpl(
+          new StringASTNodeImpl('key1', createLinePos(4, 5, 2, 3)),
+          new StringASTNodeImpl('value1', createLinePos(11, 8, 2, 10)),
+          createLinePos(4, 15, 2, 3)
+        );
+        const property2 = new PropertyASTNodeImpl(
+          new StringASTNodeImpl('key1', createLinePos(20, 5, 4, 3)),
+          new StringASTNodeImpl('value1', createLinePos(30, 8, 4, 10)),
+          createLinePos(23, 15, 4, 3)
+        );
+        // {
+        //   "key1" "value1"
+        //
+        //   "key2" "value2"
+        // }
+        const obj = new ObjectASTNodeImpl(
+          [property1, property2],
+          createStartPos(40, 5, 2)
+        );
+
+        expect(findAtOffset(obj, 21)).toBe(obj);
+      });
+    });
+  });
 });
 
 /** Creates a simple string property node. */
@@ -65,4 +178,33 @@ function createStringPropertyNode(
   const propertyNode = new PropertyASTNodeImpl(keyNode, valueNode);
 
   return propertyNode;
+}
+
+function createStartPos(
+  length: number,
+  rowEnd: number,
+  columnEnd: number
+): NodePositionImpl {
+  return new NodePositionImpl(0, length, 1, 1, rowEnd, columnEnd);
+}
+
+function createLinePos(
+  offset: number,
+  length: number,
+  rowBegin?: number,
+  columnBegin?: number
+): NodePositionImpl {
+  return new NodePositionImpl(
+    offset,
+    length,
+    rowBegin ?? 1,
+    columnBegin ?? offset + 1,
+    rowBegin ?? 1,
+    (columnBegin ?? offset + 1) + length
+  );
+}
+
+/** Asserts that the position data of a single-line node is correct. */
+function createStartLinePos(length: number): NodePositionImpl {
+  return createStartPos(length, 1, length + 1);
 }
