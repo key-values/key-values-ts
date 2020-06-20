725cb6b9eb860f38b18c5383a1e8012bbde208dd
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-06-20 23:00:35 +0200

    Fix compiler options, do clean build before publishing

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import KeyValues from './key_values';
-import { KeyValuesDocument } from './key_values';
-import * as NodeParser from './node_parser';
-import * as NodeStringifier from './node_stringifier';
-import * as ASTNodes from './ast_node';
-import * as ASTNodeImpls from './ast_node_impl';
+import { parse, stringify } from './key_values';
+export { KeyValuesDocument } from './key_values';
+export * as NodeParser from './node_parser';
+export * as NodeStringifier from './node_stringifier';
+export * as ASTNodes from './ast_node';
+export * as ASTNodeImpls from './ast_node_impl';
+
+const KeyValues = { parse, stringify };
 
 export default KeyValues;
-export {
-  KeyValuesDocument,
-  NodeParser,
-  NodeStringifier,
-  ASTNodes,
-  ASTNodeImpls,
-};
