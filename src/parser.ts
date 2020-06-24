2e981dbf1e9851231752548845ca52a02f74836c
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-06-24 16:25:34 +0200

    Move parsers to KeyValuesParser class

--- src/parser.ts
+++ src/parser.ts
@@ -14,6 +14,7 @@ import {
   kleft,
   kright,
   rep_sc,
+  Lexer,
 } from 'typescript-parsec';
 
 import {
@@ -28,154 +29,208 @@ import {
 
 import { TokenKind, lexer } from './lexer';
 
-export const TRIVIA_BASE = alt(tok(TokenKind.Space), tok(TokenKind.Comment));
-export const TRIVIA = rep_sc(TRIVIA_BASE);
-export const TRIVIA_SOME = seq(TRIVIA_BASE, TRIVIA);
-export const STRING_UNQUOTED = rule<TokenKind, StringASTNodeImpl>();
-export const STRING_QUOTED = rule<TokenKind, StringASTNodeImpl>();
-export const STRING = rule<TokenKind, StringASTNodeImpl>();
-export const KEY = rule<TokenKind, KeyASTNodeImpl>();
-export const VALUE = rule<TokenKind, ValueASTNodeImpl>();
-export const PROPERTY = rule<TokenKind, PropertyASTNodeImpl>();
-export const OBJECT = rule<TokenKind, ObjectASTNodeImpl>();
-export const KEY_VALUES = rule<TokenKind, KeyValuesASTNodeImpl>();
+export default class KeyValuesParser {
+  public lexer: Lexer<TokenKind>;
 
-function applyUnquoted(
-  value: Token<TokenKind.UnquotedString>
-): StringASTNodeImpl {
-  const pos = new NodePositionImpl(
-    value.pos.index,
-    value.text.length,
-    value.pos.rowBegin,
-    value.pos.columnBegin,
-    value.pos.rowEnd,
-    value.pos.columnEnd
-  );
+  // Non-recursive parsers
+  public space: Parser<TokenKind, unknown>;
+  public comment: Parser<TokenKind, unknown>;
+  public trivia: Parser<TokenKind, unknown>;
+  public openBrace: Parser<TokenKind, Token<TokenKind.LBrace>>;
+  public closeBrace: Parser<TokenKind, Token<TokenKind.RBrace>>;
+  public unquotedString: Parser<TokenKind, StringASTNodeImpl>;
+  public quotedString: Parser<TokenKind, StringASTNodeImpl>;
+  public string: Parser<TokenKind, StringASTNodeImpl>;
+  public key: Parser<TokenKind, KeyASTNodeImpl>;
 
-  const node = new StringASTNodeImpl(value.text, pos);
-  node.isQuoted = false;
-  return node;
-}
+  // Recursive parsers
+  public value: Parser<TokenKind, ValueASTNodeImpl>;
+  public property: Parser<TokenKind, PropertyASTNodeImpl>;
+  public object: Parser<TokenKind, ObjectASTNodeImpl>;
+  public keyValues: Parser<TokenKind, KeyValuesASTNodeImpl>;
 
-function applyQuoted(value: Token<TokenKind.QuotedString>): StringASTNodeImpl {
-  const pos = new NodePositionImpl(
-    value.pos.index,
-    value.text.length,
-    value.pos.rowBegin,
-    value.pos.columnBegin,
-    value.pos.rowEnd,
-    value.pos.columnEnd
-  );
+  constructor() {
+    this.lexer = lexer;
 
-  // Extract string value
-  let str = value.text.slice(1, value.text.length - 1);
-  // Resolve escaped quotes
-  str = str.replace(/\\"/, '"');
+    // Apply functions
+    const applyUnquotedString: (
+      value: Token<TokenKind.UnquotedString>
+    ) => StringASTNodeImpl = (value) => {
+      const pos = new NodePositionImpl(
+        value.pos.index,
+        value.text.length,
+        value.pos.rowBegin,
+        value.pos.columnBegin,
+        value.pos.rowEnd,
+        value.pos.columnEnd
+      );
 
-  return new StringASTNodeImpl(str, pos);
-}
+      // Extract string value
+      let str = value.text.slice(1, value.text.length - 1);
+      // Resolve escaped quotes
+      str = str.replace(/\\"/, '"');
 
-function applyString(value: StringASTNodeImpl): StringASTNodeImpl {
-  return value;
-}
+      return new StringASTNodeImpl(str, pos);
+    };
 
-function applyKey(value: KeyASTNodeImpl): KeyASTNodeImpl {
-  return value;
-}
+    const applyQuotedString: (
+      value: Token<TokenKind.QuotedString>
+    ) => StringASTNodeImpl = (value) => {
+      const pos = new NodePositionImpl(
+        value.pos.index,
+        value.text.length,
+        value.pos.rowBegin,
+        value.pos.columnBegin,
+        value.pos.rowEnd,
+        value.pos.columnEnd
+      );
 
-function applyValue(value: ValueASTNodeImpl): ValueASTNodeImpl {
-  return value;
-}
+      // Extract string value
+      let str = value.text.slice(1, value.text.length - 1);
+      // Resolve escaped quotes
+      str = str.replace(/\\"/, '"');
 
-function applyProperty(
-  values: [KeyASTNodeImpl, ValueASTNodeImpl]
-): PropertyASTNodeImpl {
-  const key = values[0];
-  const value = values[1];
+      return new StringASTNodeImpl(str, pos);
+    };
 
-  if (!key.pos || !value.pos) throw new Error('Missing position data.');
+    const applyString: (value: StringASTNodeImpl) => StringASTNodeImpl = (
+      value
+    ) => {
+      return value;
+    };
 
-  const pos = new NodePositionImpl(
-    key.pos.offset,
-    value.pos.offset + value.pos.length - key.pos.offset,
-    key.pos.rowBegin,
-    key.pos.columnBegin,
-    value.pos.rowEnd,
-    value.pos.columnEnd
-  );
+    const applyKey: (value: StringASTNodeImpl) => KeyASTNodeImpl = (value) => {
+      return value;
+    };
 
-  const property = new PropertyASTNodeImpl(key, value, pos);
+    const applyValue: (value: ValueASTNodeImpl) => ValueASTNodeImpl = (
+      value
+    ) => {
+      return value;
+    };
 
-  return property;
-}
+    const applyProperty: (
+      value: [KeyASTNodeImpl, ValueASTNodeImpl]
+    ) => PropertyASTNodeImpl = (values) => {
+      const key = values[0];
+      const value = values[1];
 
-function applyObject(
-  values: [
-    Token<TokenKind.LBrace>,
-    PropertyASTNodeImpl[] | undefined,
-    Token<TokenKind.RBrace>
-  ]
-): ObjectASTNodeImpl {
-  const lBrace = values[0];
-  const properties = values[1] || [];
-  const rBrace = values[2];
+      if (!key.pos || !value.pos) throw new Error('Missing position data.');
 
-  const pos = new NodePositionImpl(
-    lBrace.pos.index,
-    rBrace.pos.index + 1 - lBrace.pos.index,
-    lBrace.pos.rowBegin,
-    lBrace.pos.columnBegin,
-    rBrace.pos.rowEnd,
-    rBrace.pos.columnEnd
-  );
+      const pos = new NodePositionImpl(
+        key.pos.offset,
+        value.pos.offset + value.pos.length - key.pos.offset,
+        key.pos.rowBegin,
+        key.pos.columnBegin,
+        value.pos.rowEnd,
+        value.pos.columnEnd
+      );
 
-  const object = new ObjectASTNodeImpl(properties, pos);
-  properties.forEach((property) => (property.parent = object));
+      const property = new PropertyASTNodeImpl(key, value, pos);
 
-  return object;
-}
+      return property;
+    };
 
-function applyKeyValues(value: KeyValuesASTNodeImpl): KeyValuesASTNodeImpl {
-  return value;
-}
+    const applyObject: (
+      values: [
+        Token<TokenKind.LBrace>,
+        PropertyASTNodeImpl[] | undefined,
+        Token<TokenKind.RBrace>
+      ]
+    ) => ObjectASTNodeImpl = (values) => {
+      const lBrace = values[0];
+      const properties = values[1] || [];
+      const rBrace = values[2];
 
-/** Unquoted string */
-STRING_UNQUOTED.setPattern(apply(tok(TokenKind.UnquotedString), applyUnquoted));
-/** Quoted string */
-STRING_QUOTED.setPattern(apply(tok(TokenKind.QuotedString), applyQuoted));
-/** String */
-STRING.setPattern(apply(alt(STRING_UNQUOTED, STRING_QUOTED), applyString));
-/** Key */
-KEY.setPattern(apply(STRING, applyKey));
-/** Value */
-VALUE.setPattern(apply(alt(STRING, OBJECT), applyValue));
-/** Property */
-PROPERTY.setPattern(apply(seq(kleft(KEY, TRIVIA), VALUE), applyProperty));
-/** Object */
-OBJECT.setPattern(
-  apply(
-    seq(
-      kleft(
-        tok(TokenKind.LBrace), // A left brace ({) ...
-        TRIVIA // ...followed by some trivia...
-      ),
-      opt_sc(
-        // ...followed by an optional list of properties...
-        list_sc(
-          PROPERTY, //...containing properties...
-          TRIVIA_SOME // ...separated by space or comments.
-        )
-      ),
-      kright(
-        TRIVIA, // ...followed by some trivia...
-        tok(TokenKind.RBrace) // ...follwed by a right brace (}).
+      const pos = new NodePositionImpl(
+        lBrace.pos.index,
+        rBrace.pos.index + 1 - lBrace.pos.index,
+        lBrace.pos.rowBegin,
+        lBrace.pos.columnBegin,
+        rBrace.pos.rowEnd,
+        rBrace.pos.columnEnd
+      );
+
+      const object = new ObjectASTNodeImpl(properties, pos);
+      properties.forEach((property) => (property.parent = object));
+
+      return object;
+    };
+
+    const applyKeyValues = (value: KeyValuesASTNodeImpl) => {
+      return value;
+    };
+
+    // Non-recursive parsers
+    this.space = tok(TokenKind.Space);
+    this.comment = tok(TokenKind.Comment);
+    this.trivia = alt(this.space, this.comment);
+    this.openBrace = tok(TokenKind.LBrace);
+    this.closeBrace = tok(TokenKind.RBrace);
+    this.unquotedString = apply(
+      tok(TokenKind.UnquotedString),
+      applyUnquotedString
+    );
+    this.quotedString = apply(tok(TokenKind.QuotedString), applyQuotedString);
+    this.string = apply(
+      alt(this.unquotedString, this.quotedString),
+      applyString
+    );
+    this.key = apply(this.string, applyKey);
+
+    // Recursive parsers
+    const value = rule<TokenKind, ValueASTNodeImpl>();
+    const property = rule<TokenKind, PropertyASTNodeImpl>();
+    const object = rule<TokenKind, ObjectASTNodeImpl>();
+    const keyValues = rule<TokenKind, KeyValuesASTNodeImpl>();
+
+    value.setPattern(apply(alt(this.string, object), applyValue));
+    property.setPattern(
+      apply(seq(kleft(this.key, this.trivia), value), applyProperty)
+    );
+    object.setPattern(
+      apply(
+        seq(
+          kleft(
+            this.openBrace, // A left brace ({) ...
+            rep_sc(this.trivia) // ...followed by some trivia...
+          ),
+          opt_sc(
+            // ...followed by an optional list of properties...
+            list_sc(
+              property, //...containing properties...
+              some_sc(this.trivia) // ...separated by space or comments.
+            )
+          ),
+          kright(
+            rep_sc(this.trivia), // ...followed by some trivia...
+            this.closeBrace // ...follwed by a right brace (}).
+          )
+        ),
+        applyObject
       )
-    ),
-    applyObject
-  )
-);
-/** Key Values */
-KEY_VALUES.setPattern(apply(kmid(TRIVIA, PROPERTY, TRIVIA), applyKeyValues));
+    );
+    keyValues.setPattern(
+      apply(
+        kmid(rep_sc(this.trivia), property, rep_sc(this.trivia)),
+        applyKeyValues
+      )
+    );
+
+    this.value = value;
+    this.property = property;
+    this.object = object;
+    this.keyValues = keyValues;
+  }
+}
+
+export function some_sc<TKind, TResult>(
+  p: Parser<TKind, TResult>
+): Parser<TKind, TResult[]> {
+  return apply(seq(p, rep_sc(p)), (value: [TResult, TResult[]]) => {
+    return [value[0]].concat(value[1]);
+  });
+}
 
 export function parseWith<TResult>(
   text: string,
