b7c34718cc2a44e5f348cabf6263c43050a36f69
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-06-24 09:35:40 +0200

    Add stringify options to NodeStringifier

--- src/node_stringifier.ts
+++ src/node_stringifier.ts
@@ -5,15 +5,40 @@ import {
   ASTNode,
 } from './ast_node';
 
+export type StringifyOptions = {
+  /** The number of spaces a tab is equal to. */
+  tabSize: number;
+  /** True if spaces should be inserted instead of tabs, else false. */
+  insertSpaces: boolean;
+};
+
+/** The default stringify options. */
+const DEFAULT_OPTIONS: StringifyOptions = {
+  tabSize: 4,
+  insertSpaces: false,
+};
+
 /** Generates the specified amount of indention. */
-export function genIndent(indent?: number): string {
+export function genIndent(indent?: number, options?: StringifyOptions): string {
   if (!indent) return '';
+
+  const _options = options ?? DEFAULT_OPTIONS;
 
   let indentStr = '';
 
+  // Determine the string to indent by
+  let str = '';
+  if (_options.insertSpaces) {
+    for (let i = 0; i < _options.tabSize; i++) {
+      str += ' ';
+    }
+  } else {
+    str = '\t';
+  }
+
   for (let i = 0; i < indent; i++) {
-    // Add tab indention
-    indentStr += '\t';
+    // Add indention
+    indentStr += str;
   }
 
   return indentStr;
@@ -22,26 +47,28 @@ export function genIndent(indent?: number): string {
 /** Converts a StringASTNode to a KeyValues string. */
 export function stringifyStringNode(
   node: StringASTNode,
-  indent?: number
+  indent?: number,
+  options?: StringifyOptions
 ): string {
-  const indentStr = genIndent(indent);
+  const indentStr = genIndent(indent, options);
   return `${indentStr}"${node.value}"`;
 }
 
 /** Converts a PropertyASTNode to a KeyValues string. */
 export function stringifyPropertyNode(
   node: PropertyASTNode,
-  indent?: number
+  indent?: number,
+  options?: StringifyOptions
 ): string {
   const key = stringifyNode(node.keyNode, indent);
 
   if (node.valueNode.type == 'object') {
     // Object values are placed in a new line
-    const value = stringifyNode(node.valueNode, indent);
+    const value = stringifyNode(node.valueNode, indent, options);
     return `${key}\n${value}`;
   } else {
     // String values are placed in the same line, seperated by tabs
-    const value = stringifyNode(node.valueNode, 1);
+    const value = stringifyNode(node.valueNode, 1, options);
     return `${key}${value}`;
   }
 }
@@ -49,9 +76,10 @@ export function stringifyPropertyNode(
 /** Converts an ObjectASTNode to a KeyValues string. */
 export function stringifyObjectNode(
   node: ObjectASTNode,
-  indent?: number
+  indent?: number,
+  options?: StringifyOptions
 ): string {
-  const indentStr = genIndent(indent);
+  const indentStr = genIndent(indent, options);
 
   switch (node.properties.length) {
     case 0:
@@ -65,7 +93,7 @@ export function stringifyObjectNode(
     default: {
       // Multi-line object
       const properties = node.properties
-        .map((property) => stringifyNode(property, (indent ?? 0) + 1))
+        .map((property) => stringifyNode(property, (indent ?? 0) + 1), options)
         .join('\n');
 
       return `${indentStr}{\n${properties}\n${indentStr}}`;
@@ -74,14 +102,18 @@ export function stringifyObjectNode(
 }
 
 /** Converts an ASTNode to a KeyValues string. */
-export function stringifyNode(node: ASTNode, indent?: number): string {
+export function stringifyNode(
+  node: ASTNode,
+  indent?: number,
+  options?: StringifyOptions
+): string {
   switch (node.type) {
     case 'string':
-      return stringifyStringNode(node as StringASTNode, indent);
+      return stringifyStringNode(node as StringASTNode, indent, options);
     case 'property':
-      return stringifyPropertyNode(node as PropertyASTNode, indent);
+      return stringifyPropertyNode(node as PropertyASTNode, indent, options);
     case 'object':
-      return stringifyObjectNode(node as ObjectASTNode, indent);
+      return stringifyObjectNode(node as ObjectASTNode, indent, options);
     default:
       throw new Error(`Unexpected node type: ${node.type}.`);
   }
