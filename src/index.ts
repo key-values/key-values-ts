46776dbb467c1282bb0c7af3c1fa38c07883a985
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-06-27 15:56:59 +0200

    Add ObjectParser and Lexer to exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { parse, stringify } from './key_values';
 export { KeyValuesDocument } from './key_values';
 export * as NodeParser from './node_parser';
 export * as NodeStringifier from './node_stringifier';
+export * as Lexer from './lexer';
+export * as ObjectParser from './object_parser';
 export * as ASTNodes from './ast_node';
 export * as ASTNodeImpls from './ast_node_impl';
 
